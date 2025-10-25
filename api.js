1a65f3e7b5acdd3d03ca98518a291ff24ad7e5c1
cipherzok <cipherzok@gmail.com>
2025-10-25 14:51:57 -0600

    Player position (read only)

--- api.js
+++ api.js
@@ -1,41 +1,63 @@
 const internals = {};
 
-const mineBlocks = {};
-
-let isFirstGame = true;
-
-addHook(window, "window.Function.prototype.bind", function (value) {
-    if (value && value.remainingSounds) {
-        internals.Instance = value;
-        Object.defineProperty(internals.Instance, "game", {
-            get() {
-                return internals.game;
-            },
-            set(value) {
-                internals.game = value;
-                if (isFirstGame) {
-                    mineBlocks.gamePrototype = internals.game.__class__.prototype;
-                    listeners.firstGame();
-                    isFirstGame = false;
+addHook(window, "window.Function.prototype.bind", function (Instance) {
+    if (Instance && Instance.remainingSounds) {
+        const Main = Instance.__class__;
+        Main.Instance = new Proxy(Main.Instance, {
+            set(target, property, value) {
+                if (property === "game") {
+                    internals.game = value;
+                    listeners.newGame();
                 }
+                target[property] = value;
+                return true;
             }
         });
+        internals.Main = Main;
         return true;
     }
 });
 
+const api = {};
+
 const listeners = {};
 
-listeners.firstGame = function () {
-    api.interpretCommand = function (text) {
-        if (typeof text === "string") {
-            internals.game.interpretCommand(text, true);
-        } else {
-            console.log("Invalid string:", text);
-        }
-    }
+let isFirstGame = true;
+
+function isIntegerBetween(min, max, data) {
+    return Number.isInteger(data) && min <= data && max >= data;
 }
 
-const api = {};
+listeners.newGame = function () {
+    console.log("[API] New game");
+    if (isFirstGame) {
+        api.interpretCommand = function (text) {
+            if (typeof text === "string") {
+                internals.game.interpretCommand(text, true);
+            } else {
+                console.log("Invalid string:", text);
+            }
+        }
+        api.player = {
+            set health(value) {
+                if (isIntegerBetween(0, 20, value)) {
+                    internals.game.world.health = value
+                } else {
+                    console.log("Player's health must be an whole number between 0 and 20: ", value)
+                }
+            },
+            get health() {
+                return internals.game.world.health;
+            },
+            get x() {
+                return internals.game.world.worldX;
+            },
+            get y() {
+                return internals.game.world.worldY;
+            }
+        }
+        isFirstGame = false;
+    }
+}
 
 window.mineBlocksApi = api;
